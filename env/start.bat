45f9f199df4ca82f10856a4ff46054ce4634a477
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-02-01 14:51:23 +0900

    Wi-Fi接続のネットワークIPが取得できてなかったのを修正

--- env/start.bat
+++ env/start.bat
@@ -5,6 +5,11 @@ cd ..\cake_app\bin
 
 REM ローカルエリア接続のネットワークIPを取得
 for /f "tokens=1,2* usebackq delims=^:" %%i in (`netsh interface ipv4 show address "ローカル エリア接続" ^| findstr "IP アドレス" ^| findstr /n /r "."`) do @set NetworkIP=%%k
+
+REM Wi-Fi接続のネットワークIPを取得
+if "%NetworkIP%" == "" (
+    for /f "tokens=1,2* usebackq delims=^:" %%i in (`netsh interface ipv4 show address "Wi-Fi" ^| findstr "IP アドレス" ^| findstr /n /r "."`) do @set NetworkIP=%%k
+)
 call :Trim %NetworkIP%
 
 echo %NetworkIP% | findstr /B 192.168. > nul
