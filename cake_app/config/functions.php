7ea8f685d4a38f82fa5ec87a2d2e27a9647d81ae
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-08-30 10:29:31 +0900

    プラグインの最新化など
    インデント修正（タブ1個→スペース4個）

--- cake_app/config/functions.php
+++ cake_app/config/functions.php
@@ -6,21 +6,23 @@ use Cake\Core\Configure;
  * @param $code_key
  * @return bool|mixed
  */
-function _code($code_key) {
-	return Configure::read($code_key);
+function _code($code_key)
+{
+    return Configure::read($code_key);
 }
 
 /**
  * ランダムな文字列の生成
  * @param number $length
  */
-function create_random_str($length = 8) {
-	$str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z'));
-	$r_str = null;
-	for ($i = 0; $i < $length; $i++) {
-		$r_str .= $str[rand(0, count($str) - 1)];
-	}
-	return $r_str;
+function create_random_str($length = 8)
+{
+    $str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z'));
+    $r_str = null;
+    for ($i = 0; $i < $length; $i++) {
+        $r_str .= $str[rand(0, count($str) - 1)];
+    }
+    return $r_str;
 }
 
 
@@ -30,22 +32,22 @@ function create_random_str($length = 8) {
  *
  * @param string $plain_password
  */
-function encrypt_password($plain_password = '') {
+function encrypt_password($plain_password = '')
+{
+    // 暗号化用のメソッドを指定
+    $method = 'AES-256-CBC';
 
-	// 暗号化用のメソッドを指定
-	$method = 'AES-256-CBC';
+    // 暗号化・復元用のIVキーを作成
+    // 暗号初期化ベクトル (iv) の長さを取得
+    $iv_size = openssl_cipher_iv_length($method);
 
-	// 暗号化・復元用のIVキーを作成
-	// 暗号初期化ベクトル (iv) の長さを取得
-	$iv_size = openssl_cipher_iv_length($method);
+    // 暗号モードに対するIVの長さに合わせたキーを生成します
+    $iv = openssl_random_pseudo_bytes($iv_size);
 
-	// 暗号モードに対するIVの長さに合わせたキーを生成します
-	$iv = openssl_random_pseudo_bytes($iv_size);
+    // 暗号化
+    $encrypted_password = openssl_encrypt($plain_password, $method, _code('AdminConfig.CakeEncryptionSalt'), OPENSSL_RAW_DATA, $iv);
 
-	// 暗号化
-	$encrypted_password = openssl_encrypt($plain_password, $method, _code('AdminConfig.CakeEncryptionSalt'), OPENSSL_RAW_DATA, $iv);
-
-	return base64_encode($iv) . ':' . base64_encode($encrypted_password);
+    return base64_encode($iv) . ':' . base64_encode($encrypted_password);
 }
 
 /**
@@ -53,47 +55,16 @@ function encrypt_password($plain_password = '') {
  *
  * @param string $encrypted_password
  */
-function decrypt_password($encrypted_password = '') {
+function decrypt_password($encrypted_password = '')
+{
+    // 復元したいデータとそのIVキーを取得
+    $password_data = explode(':', $encrypted_password);
+    $iv = base64_decode($password_data[0]);
+    $encrypted = base64_decode($password_data[1]);
 
-	// 復元したいデータとそのIVキーを取得
-	$password_data = explode(':', $encrypted_password);
-	$iv = base64_decode($password_data[0]);
-	$encrypted = base64_decode($password_data[1]);
+    // 暗号化用のメソッドを指定
+    $method = 'AES-256-CBC';
 
-	// 暗号化用のメソッドを指定
-	$method = 'AES-256-CBC';
-
-	// 複合化
-	return openssl_decrypt($encrypted, $method, _code('AdminConfig.CakeEncryptionSalt'), OPENSSL_RAW_DATA, $iv);
-}
-
-/**
- * 桁数を指定した切り捨て
- * 参考：https://gotohayato.com/content/491/
- * @param unknown $value
- * @param number $precision
- * @return number
- */
-function floor_plus($value, $precision = 1) {
-	return round($value - 0.5 * pow(0.1, $precision), $precision, PHP_ROUND_HALF_UP);
-}
-
-/**
- * 文字列$haystackは$needleで始まる？
- * @param string $haystack
- * @param string $needle
- * @return boolean
- */
-function starts_with($haystack, $needle) {
-	return $needle === "" || strpos($haystack, $needle) === 0;
-}
-
-/**
- * 文字列$haystackは$needleで終わる？
- * @param string $haystack
- * @param string $needle
- * @return boolean
- */
-function ends_with($haystack, $needle) {
-	return $needle === "" || substr($haystack, - strlen($needle)) === $needle;
+    // 複合化
+    return openssl_decrypt($encrypted, $method, _code('AdminConfig.CakeEncryptionSalt'), OPENSSL_RAW_DATA, $iv);
 }
